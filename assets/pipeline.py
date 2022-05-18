eefccb5b7f2a895b79cfd13b033e50b1806d0c06
yx167 <reidxue@gmail.com>
2022-05-18 18:35:14 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.py
+++ assets/pipeline.py
@@ -2,24 +2,30 @@ import subprocess
 import os
 
 
+'''
+cmd = 'ls'
+os.system(cmd)
+val = os.popen('pwd').read()
+print(val)
+'''
+
+
 
 os.system('npm install')
 print('npm install successful')
 val = os.system('npm run build')
-
+print(val)
 
 if val == 0:
 
-
         os.system('git add ../.')
-        commit = 'git commit -m'+'COMPX341-22A-A3 Commiting from CI/CD Pipeline'
+        commit = 'git commit -m'+' "COMPX341-22A-A3 Commiting from CI/CD Pipeline"'
         os.system(commit)
+
         os.system('git push -u origin main')
         os.system('npm run start')
-        print('finsh pipeline')
+        print(commit)
 
 else:
         print('something wrong')
         os._exit(0)
-
-
